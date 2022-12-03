8158702be08beb2987e37a794a7f7cc3183a77bf
Zain <55933220+Intensifiesx@users.noreply.github.com>
2022-12-03 11:33:40 -0800

    Update BernoulliTrials.cpp

--- BernoulliTrials.cpp
+++ BernoulliTrials.cpp
@@ -2,11 +2,13 @@
 #include <math.h>
 using namespace std;
 
+// Get factorial of a number recursively
 int factorial(int n)
 {
     return n < 2 ? 1 : n * factorial(n - 1);
 }
 
+// Calculate bernoulli's theorem of probability
 double BernoulliTheorem(int n, int k, double p)
 {
     int nChooseK = factorial(n) / (factorial(k) * factorial(n - k));
