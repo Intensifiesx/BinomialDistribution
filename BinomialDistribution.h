19355fd38aca6657fb00d11788a05e99492de804
Zain <55933220+Intensifiesx@users.noreply.github.com>
2023-04-11 00:32:42 -0700

    Updated class

--- BinomialDistribution.h
+++ BinomialDistribution.h
@@ -4,40 +4,30 @@ using namespace std;
 
 class BinomialDistribution
 {
+
     double result;
     /*  Calculates factorial of a number recursively
-        factorial(int n)
-        n = factorial num to repeat */
-    int factorial(int n)
+        @param int n = factorial num to repeat
+        @return unsigned long long int = factorial of n
+    */
+    unsigned long long int factorial(int n)
     {
         return n < 2 ? 1 : n * factorial(n - 1);
     }
+
+public:
     /*  Calculates binomial distribution to find the
-        number of successes in a sequence.
-        calculateBinomial(int n, int k, double p)
-        n = # of trials
-        k = # of successes
-        p = probability of getting a success  */
-    double calculateBinomial(int n, int k, double p)
+        probability of getting k successes in n trials
+        @param int n = # of trials
+        @param int k = # of successes
+        @param double p = probability of getting a success
+        @return int = binomial distribution
+    */
+    double calcBin(int n, int k, double p)
     {
         int nChooseK = factorial(n) / (factorial(k) * factorial(n - k));
         double pPowerOfK = pow(p, k);
         double oneMinusPToPowerOfNMinusK = pow((1 - p), (n - k));
         return nChooseK * pPowerOfK * oneMinusPToPowerOfNMinusK;
     }
-
-public:
-    // Constructor
-    BinomialDistribution(int n, int k, double p)
-    {
-        this->result = calculateBinomial(n, k, p);
-    }
-    friend ostream &operator<<(ostream &out, const BinomialDistribution &b);
 };
-
-// Overloads << operator to output result
-ostream &operator<<(ostream &out, const BinomialDistribution &b)
-{
-    out << b.result;
-    return out;
-}
--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 int main()
 {
+    BinomialDistribution binomial;
     /* BinomialDistribution(int n, int k, double p)
         n = # of trials
         k = # of successes
         p = probability of getting a success */
-    cout << BinomialDistribution(6, 2, 1.0 / 3.0) << endl;
+    double result = 0;
+
+    for (int i = 10; i < 21; i++)
+        result += binomial.calcBin(20, i, 0.3);
+
+    cout << "n=20, 10<=k<=20, p=0.3: " << result << endl;
     return 0;
 }
